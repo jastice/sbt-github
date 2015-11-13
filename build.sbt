2c35115f76d6f986f22621aea54c8da0ddf3b62a
Justin Kaeser <github@accounts.justinkaeser.com>
2015-11-13 23:38:17 +0100

    an actual plugin

--- build.sbt
+++ build.sbt
@@ -9,8 +9,13 @@ scalaVersion := "2.10.4"
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.2.4" % "test" withSources() withJavadoc(),
   "org.scalacheck" %% "scalacheck" % "1.12.5" % "test" withSources() withJavadoc(),
-  "com.typesafe" %% "scalalogging-slf4j" % "1.0.1"
+  "com.typesafe" %% "scalalogging-slf4j" % "1.0.1",
+  "net.databinder.dispatch" %% "dispatch-core" % "0.11.2",
+  "io.spray" %%  "spray-json" % "1.3.2",
+  "org.scala-lang" % "scala-swing" % "2.10.+",
+  "net.databinder.dispatch" %% "dispatch-core" % "0.11.2"
 )
 
 initialCommands := "import org.gestern.sbtgithub._"
 
+sbtPlugin := true
--- src/main/scala/works/mesh/SbtGithub.scala
+++ src/main/scala/works/mesh/SbtGithub.scala
@@ -1,8 +1,46 @@
 package works.mesh
 
-import com.typesafe.scalalogging.slf4j.Logging
+import sbt._, Keys._
+import dispatch._, dispatch.Defaults._
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import spray.json._
+import DefaultJsonProtocol._
 
-object SbtGithub extends App with Logging {
+object SbtGithub extends AutoPlugin {
 
-  println("""sbt-github is ready!""")
+  override def trigger = allRequirements
+
+  object autoImport {
+    val githubToken = settingKey[String]("GitHub API token. Don't commit this in your project!")
+    val githubCreateProject = taskKey[Unit]("Create a github project and push this repository.")
+  }
+
+  import autoImport._
+
+
+  override lazy val projectSettings = Seq(
+
+    githubCreateProject := {
+
+      val create = dispatch.url("https://api.github.com/user/repos")
+        .addHeader("Authorization",s"token ${githubToken.value}")
+        .setContentType("application/json", "UTF-8")
+        .POST << s"""{ "name":${name.value} }"""
+
+      val created = Http(create OK as.String)
+
+      val gitPush = for {
+        response <- created
+      } yield {
+        val sshUrl = response.parseJson.asJsObject.getFields("ssh_url").head.convertTo[String]
+        s"git remote add origin $sshUrl".!!
+        "git push -u origin master".!!
+      }
+
+      println(Await.result(gitPush ,1.minute))
+    }
+
+  )
+
 }
